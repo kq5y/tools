c6a095abeb0b0c75591d0c3ba908737c27a6002b
tksnnx <tkser.edu@gmail.com>
2024-10-31 21:54:48 +0900

    add: top page

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap",
   },
 ];
 
@@ -35,7 +35,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <Header />
-        {children}
+        <main>
+          {children}
+        </main>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,13 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { Link } from "@remix-run/react";
+
+interface ToolRoot {
+  cat: string,
+  title: string,
+  desc: string
+}
+
+const roots: ToolRoot[] = []
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,7 +17,31 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const rootmap = roots.reduce((m, r) => {
+    (m[r.cat] = m[r.cat] || []).push(r);
+    return m;
+  }, {} as Record<string, ToolRoot[]>);
   return (
-    <div>index page</div>
+    <div>
+      <h1 className="text-2xl font-bold">Tools</h1>
+      {Object.keys(rootmap).map((cat) => (
+        <div key={cat} className="px-2 py-1">
+          <div className="text-xl font-semibold text-indigo-950">
+            /{cat}
+          </div>
+          <div className="flex flex-col">
+            {rootmap[cat].map((r) => (
+              <div key={r.title} className="flex gap-x-1 px-4 items-baseline">
+                <Link to={`/${cat}/${r.title}`} className="text-lg font-bold text-indigo-950">
+                  /{r.title}
+                </Link>
+                <span className="text-gray-500 text-sm">:</span>
+                <span className="text-gray-600 text-sm">{r.desc}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
