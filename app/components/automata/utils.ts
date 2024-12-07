e5f6364e452aba678334d731ef9bc9bf3b21997a
kq5y <tkser.edu@gmail.com>
2024-12-08 03:11:47 +0900

    fix[automata.simplest]: make dfa2simplest

--- app/components/automata/utils.ts
+++ app/components/automata/utils.ts
@@ -28,3 +28,85 @@ export const getMermaidFromTransitions = (
   }
   return res;
 };
+
+export const dfa2simplest = (
+  transitions: Transition[],
+  outputKeys: string[],
+  nodesById: { [key: number]: Transition }
+) => {
+  const simplestTransitions: Transition[] = [];
+  const groupStrings: string[] = [];
+  let equivalentGroups: number[][] = [[], []];
+  for (const tran of transitions) {
+    if (tran.final) equivalentGroups[1].push(tran.id);
+    else equivalentGroups[0].push(tran.id);
+  }
+  groupStrings.push(JSON.stringify(equivalentGroups));
+  while (true) {
+    let newGroups: number[][] = [];
+    for (const group of equivalentGroups) {
+      const newGroup: { [key: string]: number[] } = {};
+      for (const id of group) {
+        const labels = [];
+        for (const key of outputKeys) {
+          for (
+            let searchIdx = 0;
+            searchIdx < equivalentGroups.length;
+            searchIdx++
+          ) {
+            if (
+              equivalentGroups[searchIdx].includes(
+                nodesById[id].outputs[key][0]
+              )
+            ) {
+              labels.push(searchIdx);
+              break;
+            }
+          }
+        }
+        newGroup[labels.join("&&")] = newGroup[labels.join("&&")] || [];
+        newGroup[labels.join("&&")].push(id);
+      }
+      newGroups = [...newGroups, ...Object.values(newGroup)];
+    }
+    groupStrings.push(JSON.stringify(newGroups));
+    if (
+      equivalentGroups.length === newGroups.length &&
+      newGroups.every((group, i) =>
+        group.every((id) => equivalentGroups[i].includes(id))
+      )
+    )
+      break;
+    equivalentGroups = newGroups;
+  }
+  for (let groupIdx = 0; groupIdx < equivalentGroups.length; groupIdx++) {
+    simplestTransitions.push({
+      id: groupIdx,
+      node: equivalentGroups[groupIdx].join(","),
+      initial: equivalentGroups[groupIdx].some((id) => nodesById[id].initial),
+      final: equivalentGroups[groupIdx].some((id) => nodesById[id].final),
+      outputs: outputKeys.reduce(
+        (acc, key) => {
+          const target =
+            nodesById[equivalentGroups[groupIdx][0]].outputs[key][0];
+          for (
+            let searchIdx = 0;
+            searchIdx < equivalentGroups.length;
+            searchIdx++
+          ) {
+            if (equivalentGroups[searchIdx].includes(target)) {
+              acc[key] = [searchIdx];
+              break;
+            }
+          }
+          return acc;
+        },
+        {} as { [key: string]: number[] }
+      ),
+    });
+  }
+  return {
+    simplestTransitions,
+    groupStrings,
+  };
+};
--- app/routes/automata.simplest.tsx
+++ app/routes/automata.simplest.tsx
@@ -2,12 +2,11 @@ import elkLayouts from "@mermaid-js/layout-elk";
 import type { MetaFunction } from "@remix-run/cloudflare";
 import mermaid from "mermaid";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { TransitionTable, useTransitionTable } from "~/components/automata";
 import {
-  type Transition,
-  TransitionTable,
-  useTransitionTable,
-} from "~/components/automata";
-import { getMermaidFromTransitions } from "~/components/automata/utils";
+  dfa2simplest,
+  getMermaidFromTransitions,
+} from "~/components/automata/utils";
 import { getMeta } from "~/routes";
 
 export const meta: MetaFunction = () => {
@@ -56,84 +55,14 @@ export default function Simplest() {
     return getMermaidFromTransitions(transitions, outputKeys);
   }, [transitions, outputKeys]);
   const getPreviewSimplestAutomataMermaid = useCallback(() => {
-    const simplestTransitions: Transition[] = [];
-    const groupStrings: string[] = [];
-    const nodesById: { [key: number]: Transition } = {};
-    const nodesByName: { [key: string]: number } = {};
-    let equivalentGroups: number[][] = [[], []];
-    for (const tran of transitions) {
-      nodesById[tran.id] = tran;
-      nodesByName[tran.node] = tran.id;
-      if (tran.final) equivalentGroups[1].push(tran.id);
-      else equivalentGroups[0].push(tran.id);
-    }
-    groupStrings.push(JSON.stringify(equivalentGroups));
-    while (true) {
-      let newGroups: number[][] = [];
-      for (const group of equivalentGroups) {
-        const newGroup: { [key: string]: number[] } = {};
-        for (const id of group) {
-          const labels = [];
-          for (const key of outputKeys) {
-            for (
-              let searchIdx = 0;
-              searchIdx < equivalentGroups.length;
-              searchIdx++
-            ) {
-              if (
-                equivalentGroups[searchIdx].includes(
-                  nodesById[id].outputs[key][0]
-                )
-              ) {
-                labels.push(searchIdx);
-                break;
-              }
-            }
-          }
-          newGroup[labels.join("&&")] = newGroup[labels.join("&&")] || [];
-          newGroup[labels.join("&&")].push(id);
-        }
-        newGroups = [...newGroups, ...Object.values(newGroup)];
-      }
-      groupStrings.push(JSON.stringify(newGroups));
-      if (
-        equivalentGroups.length === newGroups.length &&
-        newGroups.every((group, i) =>
-          group.every((id) => equivalentGroups[i].includes(id))
-        )
-      )
-        break;
-      equivalentGroups = newGroups;
-    }
+    const { simplestTransitions, groupStrings } = dfa2simplest(
+      transitions,
+      outputKeys,
+      nodesById
+    );
     setEquivalentGroupConverts(groupStrings);
-    for (let groupIdx = 0; groupIdx < equivalentGroups.length; groupIdx++) {
-      simplestTransitions.push({
-        id: groupIdx,
-        node: equivalentGroups[groupIdx].join(","),
-        initial: equivalentGroups[groupIdx].some((id) => nodesById[id].initial),
-        final: equivalentGroups[groupIdx].some((id) => nodesById[id].final),
-        outputs: outputKeys.reduce(
-          (acc, key) => {
-            const target =
-              nodesById[equivalentGroups[groupIdx][0]].outputs[key][0];
-            for (
-              let searchIdx = 0;
-              searchIdx < equivalentGroups.length;
-              searchIdx++
-            ) {
-              if (equivalentGroups[searchIdx].includes(target)) {
-                acc[key] = [searchIdx];
-                break;
-              }
-            }
-            return acc;
-          },
-          {} as { [key: string]: number[] }
-        ),
-      });
-    }
     return getMermaidFromTransitions(simplestTransitions, outputKeys);
-  }, [transitions, outputKeys]);
+  }, [transitions, outputKeys, nodesById]);
   const generateSimplest = async () => {
     if (previewAutomataRef.current && previewSimplestAutomataRef.current) {
       const preview = getPreviewAutomataMermaid();
