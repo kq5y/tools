4805727b7b1dbf46d5a6cc069fb392ccc9fea6d5
kq5y <tkser.edu@gmail.com>
2024-12-08 17:58:55 +0900

    add[automata.nfa2dfa]: nfa2dfa

--- app/components/automata/utils.ts
+++ app/components/automata/utils.ts
@@ -2,7 +2,8 @@ import type { Transition } from "./types";
 
 export const getMermaidFromTransitions = (
   trans: Transition[],
-  outputKeys: string[]
+  outputKeys: string[],
+  useId = false
 ) => {
   let res = "graph LR;\nstyle start fill:none, stroke:none;\nstart(( ));\n";
   let initialId = -1;
@@ -10,8 +11,8 @@ export const getMermaidFromTransitions = (
   for (const tran of trans) {
     if (tran.initial) initialId = tran.id;
     nodes[tran.node] = tran.id;
-    if (tran.final) res += `${tran.id}(((${tran.node})));\n`;
-    else res += `${tran.id}((${tran.node}));\n`;
+    if (tran.final) res += `${tran.id}(((${useId ? tran.id : tran.node})));\n`;
+    else res += `${tran.id}((${useId ? tran.id : tran.node}));\n`;
   }
   res += `start --> ${initialId};\n`;
   for (const tran of trans) {
@@ -109,4 +110,108 @@ export const dfa2simplest = (
     simplestTransitions,
     groupStrings,
   };
+};
+
+export const nfa2dfa = (
+  transitions: Transition[],
+  outputKeys: string[],
+  nodesById: { [key: number]: Transition }
+) => {
+  let initialId = -1;
+  const finalIds: number[] = [];
+  const researchableKeys: { [key: number]: number[] } = {};
+  const dfsKeys = (start: number, current: number, visited: Set<number>) => {
+    visited.add(current);
+    for (const neighbor of nodesById[current].outputs.ε || []) {
+      if (!visited.has(neighbor)) {
+        dfsKeys(start, neighbor, visited);
+      }
+    }
+  };
+  for (const tran of transitions) {
+    if (tran.initial) initialId = tran.id;
+    if (tran.final) finalIds.push(tran.id);
+    const visited = new Set<number>();
+    dfsKeys(tran.id, tran.id, visited);
+    researchableKeys[tran.id] = Array.from(visited).toSorted((a, b) => a - b);
+  }
+  const outputs = transitions.reduce(
+    (acc, tran) => {
+      acc[tran.id] = outputKeys.reduce(
+        (oacc, key) => {
+          if (key === "ε") return oacc;
+          const out = new Set<number>(
+            researchableKeys[tran.id].flatMap((ekey) =>
+              nodesById[ekey].outputs[key].flatMap((o) => researchableKeys[o])
+            )
+          );
+          oacc[key] = Array.from(out).toSorted((a, b) => a - b);
+          return oacc;
+        },
+        {} as { [key: string]: number[] }
+      );
+      return acc;
+    },
+    {} as { [id: number]: { [key: string]: number[] } }
+  );
+  let dfaNodeIdByNames: { [key: string]: number } = {};
+  let nowKeysWaits: number[][] = [];
+  const addWaits = (ids: number[]) => {
+    const name =
+      ids.length === 0
+        ? "nothing"
+        : `\{${ids.map((id) => nodesById[id].node).join(",")}\}`;
+    if (Object.keys(dfaNodeIdByNames).includes(name)) return;
+    dfaNodeIdByNames[name] = nowKeysWaits.length;
+    nowKeysWaits.push(ids);
+    for (const key of outputKeys) {
+      if (key !== "ε") {
+        const out = new Set<number>(ids.flatMap((id) => outputs[id][key]));
+        addWaits(Array.from(out).toSorted((a, b) => a - b));
+      }
+    }
+  };
+  addWaits(researchableKeys[initialId]);
+  if (Object.keys(dfaNodeIdByNames).includes("nothing")) {
+    nowKeysWaits = [...nowKeysWaits.filter((wait) => wait.length !== 0), []];
+    dfaNodeIdByNames = nowKeysWaits.reduce(
+      (acc, wait, index) => {
+        const name =
+          wait.length === 0
+            ? "nothing"
+            : `\{${wait.map((id) => nodesById[id].node).join(",")}\}`;
+        acc[name] = index;
+        return acc;
+      },
+      {} as { [key: string]: number }
+    );
+  }
+  const dfaTransitions = nowKeysWaits.map((nowKeys, index) => {
+    const name =
+      nowKeys.length === 0
+        ? "nothing"
+        : `\{${nowKeys.map((id) => nodesById[id].node).join(",")}\}`;
+    return {
+      id: dfaNodeIdByNames[name],
+      node: name,
+      initial: index === 0,
+      final: nowKeys.some((id) => finalIds.includes(id)),
+      outputs: outputKeys.reduce(
+        (acc, key) => {
+          if (key === "ε") return acc;
+          const out = Array.from(
+            new Set<number>(nowKeys.flatMap((id) => outputs[id][key]))
+          ).toSorted((a, b) => a - b);
+          const out_name =
+            out.length === 0
+              ? "nothing"
+              : `\{${out.map((id) => nodesById[id].node).join(",")}\}`;
+          acc[key] = [dfaNodeIdByNames[out_name]];
+          return acc;
+        },
+        {} as { [key: string]: number[] }
+      ),
+    };
+  });
+  return { dfaTransitions };
 };
